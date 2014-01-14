e9b0525295ac87ec9a1508aa9aff212c180ce7f6
yuly-roberman <yuli.roberman@kaltura.com>
2014-01-14 13:46:10 +0200

    replace ismc name in the manifest

--- plugins/transcoding/ismIndex/lib/kIsmIndexEventsConsumer.php
+++ plugins/transcoding/ismIndex/lib/kIsmIndexEventsConsumer.php
@@ -1,20 +1,61 @@
 <?php
-class kIsmIndexEventsConsumer implements kObjectChangedEventConsumer
+class kIsmIndexEventsConsumer implements kBatchJobStatusEventConsumer
 {	
-	/* (non-PHPdoc)
-	 * @see kObjectChangedEventConsumer::objectChanged()
-	 */
-	public function objectChanged(BaseObject $object, array $modifiedColumns) 
-	{
 
+	/* (non-PHPdoc)
+	 * @see kBatchJobStatusEventConsumer::shouldConsumeJobStatusEvent()
+	 */
+	public function shouldConsumeJobStatusEvent(BatchJob $dbBatchJob)
+	{
+		if(	$dbBatchJob->getStatus() == BatchJob::BATCHJOB_STATUS_FINISHED
+			&& $dbBatchJob->getJobType() == BatchJobType::CONVERT
+			&& $dbBatchJob->getJobSubType() == IsmIndexPlugin::getApiValue(IsmIndexConversionEngineType::ISM_MANIFEST)
+		)
+			return true;
+		else	
+			return false;
 	}
 
 	/* (non-PHPdoc)
-	 * @see kObjectChangedEventConsumer::shouldConsumeChangedEvent()
+	 * @see kBatchJobStatusEventConsumer::updatedJob()
 	 */
-	public function shouldConsumeChangedEvent(BaseObject $object, array $modifiedColumns) 
-	{
-	}
+	public function updatedJob(BatchJob $dbBatchJob)
+	{ 
+		// replacing the ismc file name in the ism file
+		$ismcOldName = null;
 
+		$fileSyncDescriptors = $dbBatchJob->getData()->getExtraDestFileSyncs();		
+		foreach ($fileSyncDescriptors as $fileSyncDescriptor) 
+		{
+			if($fileSyncDescriptor->getFileSyncObjectSubType() == flavorAsset::FILE_SYNC_ASSET_SUB_TYPE_ISMC)
+			{
+				$ismcOldName = basename($fileSyncDescriptor->getFileSyncLocalPath());
+				break;
+			}
+		}		
+		$flavorAsset = assetPeer::retrieveById($dbBatchJob->getFlavorAssetId());
+		if($flavorAsset && $ismcOldName)
+		{		
+			$ismFileSyncKey = $flavorAsset->getSyncKey(flavorAsset::FILE_SYNC_ASSET_SUB_TYPE_ISM);
+			$ismcFileSyncKey = $flavorAsset->getSyncKey(flavorAsset::FILE_SYNC_ASSET_SUB_TYPE_ISMC);
+			
+			$ismcNewName = basename(kFileSyncUtils::getLocalFilePathForKey($ismcFileSyncKey));
+			KalturaLog::debug("Editing ISM [$ismcOldName] to [$ismcNewName]");
+			
+			$ismPath = kFileSyncUtils::getLocalFilePathForKey($ismFileSyncKey);
+			$ismContent = file_get_contents($ismPath);
+			$ismContent = str_replace("content=\"$ismcOldName\"", "content=\"$ismcNewName\"", $ismContent);
+	
+			$bytesWritten = file_put_contents($ismPath, $ismContent);
+			if(!$bytesWritten)
+				KalturaLog::err("Failed to update file [$ismPath]");
+		}
+		else
+		{
+			KalturaLog::err("Failed to replace ismc old path in ism manifest");
+		}
+			
+		return true;
+	}
 
 }
